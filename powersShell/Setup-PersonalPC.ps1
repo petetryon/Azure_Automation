7e46455483d50a514d80f522d5746cb3c9ebca28
Pete Tryon <hacker6xya@msftopenhack7125ops.onmicrosoft.com>
2024-01-12 10:09:05 -0500

    First Commit

---
+++ powersShell/Setup-PersonalPC.ps1
@@ -0,0 +1,19 @@
+﻿# Choco wrapper
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+function Install-ChocoPackage($packageName) {
+    $sw = Measure-Command {
+        Write-Host "Installing/upgrading" $packageName "from Chocolatey" -ForegroundColor DarkGreen -NoNewline
+        choco upgrade $packageName -y -source https://chocolatey.org/api/v2/
+    }
+
+    Write-Host " -" $sw.TotalSeconds "seconds" -ForegroundColor DarkGray
+}
+
+Write-Host "Installing Chocolatey" -foregroundcolor DarkGreen
+Invoke-Expression ((New-Object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Install-ChocoPackage "GoogleChrome"
+Install-ChocoPackage "notepadplusplus"
+Install-ChocoPackage "7zip.install"
+Install-ChocoPackage "Steam"
+Install-ChocoPackage "windirstat"
